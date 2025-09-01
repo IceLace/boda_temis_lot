7c9b016b1c770b212fc3c87a7dcfd9f7da5d0ac6
IceLace <icelace6@gmail.com>
2025-09-01 00:05:28 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -389,7 +389,7 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2025/09/15', function(event) {
+        $('#clock').countdown('2025/09/15 15:00:00', function(event) {
             var $this = $(this).html(event.strftime(''
             + '<div class="box"><div>%D</div> <span>Días</span> </div>'
             + '<div class="box"><div>%H</div> <span>Horas</span> </div>'
